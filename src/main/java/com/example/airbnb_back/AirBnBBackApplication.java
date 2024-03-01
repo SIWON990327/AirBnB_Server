4766aed57d76532fd7808ea50778d5f2a55ffe15
SIWON990327 <pos06058@naver.com>
2024-03-01 13:05:12 +0900

    init

---
+++ src/main/java/com/example/airbnb_back/AirBnBBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnb_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnBBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirBnBBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airbnb_back/AirBnBBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airbnb_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnBBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
